c118ea42330a4f8517269d5d9fddea9ad6b74204
Ulkar <ulkar.ahmadli@gmail.com>
2026-02-02 23:33:13 +0400

    Improve output message after review feedback

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -5,7 +5,7 @@ package org.example;
 
 public class App {
     public String getGreeting() {
-        return "Hi.";
+        return "Gradle collaborative workflow completed successfully..";
     }
 
     public static void main(String[] args) {
